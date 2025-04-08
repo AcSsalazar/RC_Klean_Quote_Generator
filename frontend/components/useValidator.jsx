7ab349b5bee85be671d73aae9f7782868d9bb5a1
Esteban Gomez <esgomezhe@unal.edu.co>
2025-04-08 15:06:39 -0500

    Actualización de la base de datos

--- frontend/components/useValidator.jsx
+++ frontend/components/useValidator.jsx
@@ -12,14 +12,14 @@ const useValidation = (initialState) => {
       const equipmentValid = fields.equantity.every(item => 
         (!item.name.field && !item.quantity.field && !item.option_value.field) || // Empty item
         (item.name.validate === "true" && 
-         item.quantity.validate === "true" && 
+        item.quantity.validate === "true" && 
          (item.validOptions.length === 0 || item.option_value.validate === "true")) // Option required only if options exist
       );
       const areasValid = fields.areas.every(area => 
         (!area.name.field && !area.square_feet.field && !area.floor_type.field) || // Empty area
         (area.name.validate === "true" && 
-         area.square_feet.validate === "true" && 
-         area.floor_type.validate === "true")
+        area.square_feet.validate === "true" && 
+        area.floor_type.validate === "true")
       );
       return equipmentValid && areasValid;
     } else { // Step 3
